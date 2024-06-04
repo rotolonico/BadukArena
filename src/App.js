493bbb466e5a94c0a06a2abb0f8bb777ac6f7922
Domenico Rotolo <rotolonico@gmail.com>
2024-06-04 16:59:51 +0200

    remove hello world react

--- src/App.js
+++ src/App.js
@@ -5,17 +5,8 @@ import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Button variant="contained">Hello world</Button>
-      </header>
-    </div>
+      <Button variant="contained">Hello world</Button>
   );
 }
-
 
 export default App;
