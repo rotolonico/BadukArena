a16cd1545aaaa7342d97fa6b19bfc4b909912332
Letizia Grasso <letiziagrasso01@gmail.com>
2024-06-08 12:22:37 +0200

    Material UI - Front-end
    - Aggiunta la libreria react-router-dom per gestire le rotte nella web app.
    - Migliorati i componenti AuthComponent e ChatComponent utilizzando i componenti di Material UI.
    - Aggiunta AppBar e Toolbar al layout principale per una navigazione

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import AuthComponent from "./AuthComponent";
 import ChatComponent from "./ChatComponent";
-
+import  { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container, AppBar, Toolbar, Typography, Button} from "@mui/material";
 
 function App() {
-  return (<>
-      <AuthComponent></AuthComponent>
-      <ChatComponent></ChatComponent>
-    </>
+  return (<Router>
+          <Container>
+              <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Baduk Arena
+                </Typography>
+                <Button color="inherit" href="/auth">Login</Button>
+                <Button color="inherit" href="/chat">Chat</Button>
+              </Toolbar>
+            </AppBar>
+              <Routes>
+                <Route path="/auth" element={<AuthComponent />} />
+                <Route path="/chat" element={<ChatComponent />} />
+              </Routes>
+            </Container>
+      </Router>
   );
-}
+};
 
 export default App;
