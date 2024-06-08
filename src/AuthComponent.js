f0f840450d047877ac8dc563f4bdbd1eeb436c7f
Letizia Grasso <letiziagrasso01@gmail.com>
2024-06-08 19:47:34 +0200

    Visibilità Password in Login
    Ho utilizzato lo stato per gestire la password nel componente utilizzando useState. Ho aggiunto un campo di input per la password utilizzando il componente TextField di Material-UI.
    Ho impostato il tipo di input del campo password su "password" affinché il testo inserito sia nascosto.
    Ho gestito il valore del campo password utilizzando lo stato e l'evento onChange per aggiornarlo dinamicamente.
    Ho personalizzato l'aspetto del campo password utilizzando InputLabelProps e InputProps per modificare il colore del testo e dello sfondo.
    Ho incluso un'icona per consentire all'utente di mostrare o nascondere la password.
    Ho scaricato e utilizzato il pacchetto @mui/icons-material per importare le icone Visibility e VisibilityOff necessarie per la visualizzazione della password.

--- src/AuthComponent.js
+++ src/AuthComponent.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { register, login, logout, getProtectedData } from './api';
 import {TextField,Button,Typography,Box, Container} from "@mui/material";
+import { IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const AuthComponent = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleRegister = async () => {
         try {
@@ -44,75 +47,91 @@ const AuthComponent = () => {
         }
     };
 
-    return (
-        <Container maxWidth="sm">
-            <Box mt={5} display="flex" flexDirection="column" alignItems="center"  p={3} bgcolor="#262424" boxShadow={3} border={`3px solid  #ccc`}  borderRadius={10}>
-               <Typography variant="h4" component="h2" gutterBottom color="secondary">Login</Typography>
-                <TextField
-                    label="Username"
-                    value={username}
-                    fullWidth
-                    onChange={(e) => setUsername(e.target.value)}
-                    margin="normal"
-                    variant="outlined"
-                    InputLabelProps={{
-                        style: { color: 'white' },
-                    }}
-                    InputProps={{
-                        style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
-                        classes: {
-                            root: 'textfield-root',
-                            focused: 'textfield-focused',
-                        },
-                        className: 'chat-textfield',
-                    }}/>
-                <TextField
-                    label="Email"
-                    value={email}
-                    fullWidth
-                    onChange={(e) => setEmail(e.target.value)}
-                    margin="normal"
-                    variant="outlined"
-                    InputLabelProps={{
-                        style: { color: 'white' },
-                    }}
-                    InputProps={{
-                        style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
-                        classes: {
-                            root: 'textfield-root',
-                            focused: 'textfield-focused',
-                        },
-                        className: 'chat-textfield',
-                    }}/>
-                <TextField
-                    label="Password"
-                    value={password}
-                    fullWidth
-                    onChange={(e) => setPassword(e.target.value)}
-                    margin="normal"
-                    variant="outlined"
-                    InputLabelProps={{
-                        style: { color: 'white' },
-                    }}
-                    InputProps={{
-                        style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
-                        classes: {
-                            root: 'textfield-root',
-                            focused: 'textfield-focused',
-                        },
-                        className: 'chat-textfield',
-                    }}/>
-                <Box mt={2} style={{ paddingRight: '10px' }}>
-                    <Button variant="contained" color="secondary" onClick={handleRegister}>Registrazione</Button>
-                    <Button variant="contained" color="secondary" onClick={handleLogin} style={{ marginLeft: '5px' }}>Login</Button>
-                    <Button variant="contained" onClick={handleLogout} style={{ marginLeft: '5px' }} color="secondary">Logout</Button>
-                    <Button variant="contained" onClick={handleGetProtectedData} style={{ marginLeft: '5px' }} color="secondary">Il tuo account</Button>
-                </Box>
+        return (
+            <Container maxWidth="sm">
+                <Box mt={5} display="flex" flexDirection="column" alignItems="center" p={3} bgcolor="#262424" boxShadow={3} border={`3px solid #ccc`} borderRadius={10}>
+                    <Typography variant="h4" component="h2" gutterBottom color="secondary">Login</Typography>
+                    <TextField
+                        label="Username"
+                        value={username}
+                        fullWidth
+                        onChange={(e) => setUsername(e.target.value)}
+                        margin="normal"
+                        variant="outlined"
+                        InputLabelProps={{
+                            style: { color: 'white' },
+                        }}
+                        InputProps={{
+                            style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
+                            classes: {
+                                root: 'textfield-root',
+                                focused: 'textfield-focused',
+                            },
+                            className: 'chat-textfield',
+                        }}
+                    />
+                    <TextField
+                        label="Email"
+                        value={email}
+                        fullWidth
+                        onChange={(e) => setEmail(e.target.value)}
+                        margin="normal"
+                        variant="outlined"
+                        InputLabelProps={{
+                            style: { color: 'white' },
+                        }}
+                        InputProps={{
+                            style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
+                            classes: {
+                                root: 'textfield-root',
+                                focused: 'textfield-focused',
+                            },
+                            className: 'chat-textfield',
+                        }}
+                    />
+                    <TextField
+                        label="Password"
+                        value={password}
+                        type={showPassword ? 'text' : 'password'}
+                        fullWidth
+                        onChange={(e) => setPassword(e.target.value)}
+                        margin="normal"
+                        variant="outlined"
+                        InputLabelProps={{
+                            style: { color: 'white' },
+                        }}
+                        InputProps={{
+                            style: { color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.1)' },
+                            classes: {
+                                root: 'textfield-root',
+                                focused: 'textfield-focused',
+                            },
+                            className: 'chat-textfield',
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        onMouseDown={(e) => e.preventDefault()}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Box mt={2} style={{ paddingRight: '10px' }}>
+                        <Button variant="contained" color="secondary" onClick={handleRegister}>Registrazione</Button>
+                        <Button variant="contained" color="secondary" onClick={handleLogin} style={{ marginLeft: '5px' }}>Login</Button>
+                        <Button variant="contained" onClick={handleLogout} style={{ marginLeft: '5px' }} color="secondary">Logout</Button>
+                        <Button variant="contained" onClick={handleGetProtectedData} style={{ marginLeft: '5px' }} color="secondary">Il tuo account</Button>
+                    </Box>
 
-                {message && <Typography variant="body1" mt={2}>{message}</Typography>}
-            </Box>
-        </Container>
-    );
+                    {message && <Typography variant="body1" mt={2}>{message}</Typography>}
+                </Box>
+            </Container>
+        );
 };
 
 export default AuthComponent;
