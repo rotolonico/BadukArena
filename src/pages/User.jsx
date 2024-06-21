f74d70629548d6e054cb622c62519cc2be086b39
Domenico Rotolo <rotolonico@gmail.com>
2024-06-21 11:29:11 +0200

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -61,62 +61,61 @@ const User = () => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <div className={classes.root}>
-                <Box sx={{p: 3, backgroundColor: '#f4f6f8', borderRadius: 2, boxShadow: 3}}>
-                    <Typography variant="h4" gutterBottom sx={{mb: 2}}>
-                        Games List
-                    </Typography>
-                    {loading ? (
-                        <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                            <CircularProgress/>
-                        </Box>
+
+            <Box sx={{p: 3, backgroundColor: '#f4f6f8', borderRadius: 2, boxShadow: 3}}>
+                <Typography variant="h4" gutterBottom sx={{mb: 2}}>
+                    Games List
+                </Typography>
+                {loading ? (
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                        <CircularProgress/>
+                    </Box>
+                ) : (
+                    games.length === 0 ? (
+                        <Typography variant="body1">
+                            No games found
+                        </Typography>
                     ) : (
-                        games.length === 0 ? (
-                            <Typography variant="body1">
-                                No games found
-                            </Typography>
-                        ) : (
-                            <TableContainer component={Paper}>
-                                <Table sx={{minWidth: 650}}>
-                                    <TableHead sx={{backgroundColor: '#1976d2'}}>
-                                        <TableRow>
-                                            <TableCell align="center"
-                                                       sx={{color: 'white', fontWeight: 'bold'}}>Date</TableCell>
-                                            <TableCell align="center"
-                                                       sx={{color: 'white', fontWeight: 'bold'}}>Result</TableCell>
-                                            <TableCell align="center" sx={{color: 'white', fontWeight: 'bold'}}>White
-                                                Player</TableCell>
-                                            <TableCell align="center" sx={{color: 'white', fontWeight: 'bold'}}>Black
-                                                Player</TableCell>
+                        <TableContainer component={Paper}>
+                            <Table sx={{minWidth: 650}}>
+                                <TableHead sx={{backgroundColor: '#1976d2'}}>
+                                    <TableRow>
+                                        <TableCell align="center"
+                                                   sx={{color: 'white', fontWeight: 'bold'}}>Date</TableCell>
+                                        <TableCell align="center"
+                                                   sx={{color: 'white', fontWeight: 'bold'}}>Result</TableCell>
+                                        <TableCell align="center" sx={{color: 'white', fontWeight: 'bold'}}>White
+                                            Player</TableCell>
+                                        <TableCell align="center" sx={{color: 'white', fontWeight: 'bold'}}>Black
+                                            Player</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {games.map((game, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell align="center" sx={{fontSize: '1rem'}}>
+                                                {new Date(game.date).toLocaleDateString()}
+                                            </TableCell>
+                                            <TableCell align="center" sx={{
+                                                fontSize: '1rem',
+                                                color: game.result === "B" ? 'green' : 'red'
+                                            }}>
+                                                {game.result === "B" ? "Black Wins" : "White Wins"}
+                                            </TableCell>
+                                            <TableCell align="center" sx={{fontSize: '1rem'}}>
+                                                {game.white.username}
+                                            </TableCell>
+                                            <TableCell align="center" sx={{fontSize: '1rem'}}>
+                                                {game.black.username}
+                                            </TableCell>
                                         </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {games.map((game, index) => (
-                                            <TableRow key={index}>
-                                                <TableCell align="center" sx={{fontSize: '1rem'}}>
-                                                    {new Date(game.date).toLocaleDateString()}
-                                                </TableCell>
-                                                <TableCell align="center" sx={{
-                                                    fontSize: '1rem',
-                                                    color: game.result === "B" ? 'green' : 'red'
-                                                }}>
-                                                    {game.result === "B" ? "Black Wins" : "White Wins"}
-                                                </TableCell>
-                                                <TableCell align="center" sx={{fontSize: '1rem'}}>
-                                                    {game.white.username}
-                                                </TableCell>
-                                                <TableCell align="center" sx={{fontSize: '1rem'}}>
-                                                    {game.black.username}
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        )
-                    )}
-                </Box>
-            </div>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )
+                )}
+            </Box>
         </ThemeProvider>
     );
 }
