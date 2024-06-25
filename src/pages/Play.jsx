ccb6e78c290193b1eb683dff55715295f9101bed
Domenico Rotolo <rotolonico@gmail.com>
2024-06-25 16:23:48 +0200

    bugfix

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -135,6 +135,7 @@ const Play = () => {
         socketRef.current.socketListenGameOver((rst) => {
             setResult(rst);
             setGameState(GameState.FINISHED);
+            console.log(rst.winner);
             if (rst.winner === yourColorRef.current) {
                 document.getElementById("gameWin").play();
             } else {
